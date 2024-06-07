7874a31926483c247e1aa4fbca48a27a23c028f7
Babatunde Balogun <balogunbabatunde42@gmail.com>
2024-06-07 18:03:48 +0100

    app jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import About from './About/About'
+import Funfact from './Funfact/Funfact'
+
+function App() {
+
+  return (
+    <div>
+      <About/>
+      <Funfact/>
+    </div>
+  )
+}
+
+export default App
